ce36620588bddcc357240d029c608726adf2830d
KaiRibeiro <kaiquetrabalhosescola@gmail.com>
2020-08-21 23:52:13 -0300

    Updates

---
+++ precache-manifest.734452655169d63a06bb2ccd7f2c23a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb99e11f63603559482577cde008f46e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9232a27fe9068b0d9ce7",
+    "url": "/static/css/main.18bc3a6e.chunk.css"
+  },
+  {
+    "revision": "e9e49726cebbca030eba",
+    "url": "/static/js/2.0738edbd.chunk.js"
+  },
+  {
+    "revision": "bdcfdbef37b771299e3e4252a0d10375",
+    "url": "/static/js/2.0738edbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9232a27fe9068b0d9ce7",
+    "url": "/static/js/main.21012338.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "10a2f5bfd2d1d9b012f0d3cef6701eaf",
+    "url": "/static/media/face.10a2f5bf.png"
+  },
+  {
+    "revision": "ada7af0c9551e7f4eb5141eb4469febf",
+    "url": "/static/media/facebook.ada7af0c.png"
+  },
+  {
+    "revision": "09e6e18849731ca1424b17cb9485d91e",
+    "url": "/static/media/github.09e6e188.png"
+  },
+  {
+    "revision": "fb676fd135b0b398364f74aa727fab4b",
+    "url": "/static/media/linkedin.fb676fd1.png"
+  },
+  {
+    "revision": "64446d8dd20dd1dea4c81e415f684397",
+    "url": "/static/media/me.64446d8d.png"
+  },
+  {
+    "revision": "cde242d6a21b8690f9b595c41bcb13b2",
+    "url": "/static/media/powered.cde242d6.png"
+  }
+]);
